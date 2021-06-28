b235a264e4f452264b75ebc1a4d700f7eb4705b4
KareeMagdyy <karim67899@gmail.com>
2021-06-28 15:34:55 +0200

    adding h1s

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('section');
+const body = document.querySelector('body');
 
 for (let i = 1; i <= 30; i++){
     const btn = document.createElement('button');
@@ -6,7 +7,16 @@ for (let i = 1; i <= 30; i++){
     container.appendChild(btn);
 }
 
+for (let i = 1; i <= 10; i++){
+    const h1Container = document.createElement('div');
+    const h1s = document.createElement('h1');
+    h1s.innerText = `Click Me!`;
+    body.appendChild(h1Container);
+    h1Container.appendChild(h1s);
+}
+
 const buttons = document.querySelectorAll('button');
+const h1s = document.querySelectorAll('h1');
 
 function randomColor(){
     const r = Math.floor(Math.random() * 256);
@@ -24,6 +34,10 @@ function colorize() {
 
 
 for (let button of buttons){
-    button.addEventListener('click' , colorize)
+    button.addEventListener('click' , colorize);
+}
+
+for (let h1 of h1s){
+    h1.addEventListener('click', colorize);
 }
 
